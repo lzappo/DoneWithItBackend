107c7e81b202ad76a033714e278b0e5dedf0e333
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-14 19:58:50 -0600

    ViewImageScreen created

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
