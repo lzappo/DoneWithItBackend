75e1e188f4332bf39ac72a02b55f07f819cc6e52
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-16 18:31:32 -0600

    details listing screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -5,20 +5,7 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
 
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
