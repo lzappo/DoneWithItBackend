671ef079a32b952245563ce643ad3d8210fd3a91
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-17 19:45:36 -0600

    messages screen created

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,11 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppButton from "./app/components/AppButton";
+import React from "react";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <MessagesScreen />;
 }
