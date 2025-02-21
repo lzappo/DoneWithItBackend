b66b103424d6f710f49c9dbf45c65f8cd64383e4
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-20 18:33:33 -0600

    extending ListItem and joes implementation

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -3,10 +3,16 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <MessagesScreen />
+      <Screen>
+        <ListItem title="my title" ImageComponent={<Icon name="email" />} />
+      </Screen>
     </GestureHandlerRootView>
   );
 }
--- DoneWithIt/app/components/ListItem.js
+++ DoneWithIt/app/components/ListItem.js
@@ -11,7 +11,14 @@ import ReanimatedSwipeable from "react-native-gesture-handler/ReanimatedSwipeabl
 
 import colors from "../config/colors";
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  ImageComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <ReanimatedSwipeable
       renderRightActions={renderRightActions}
@@ -21,10 +28,11 @@ function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
     >
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -36,6 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
