f3aa5a8b13fc459f87c158eb333352c1caa4e79e
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-21 20:37:04 -0600

    AppTextInput component created

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -8,11 +8,14 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ListingsScreen />
+      <Screen>
+        <AppTextInput icon="email" placeholder="Username" />
+      </Screen>
     </GestureHandlerRootView>
   );
 }
