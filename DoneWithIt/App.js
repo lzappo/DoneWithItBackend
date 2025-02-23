853abc6b4d9ea85280bd297e3123f497d3bfa893
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-23 16:55:32 -0600

    RegisterScreen created

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -5,11 +5,12 @@ import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 import { useState } from "react";
 import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <LoginScreen />
+      <RegisterScreen />
     </GestureHandlerRootView>
   );
 }
